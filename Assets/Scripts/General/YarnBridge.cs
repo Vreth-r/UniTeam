18cbfc02a90bda9b21b885ce6e37a4f11a1582e5
Vreth <michaellatka49@gmail.com>
2026-01-06 11:13:20 -0500

    quiz logic yarn helper

--- Assets/Scripts/General/YarnBridge.cs
+++ Assets/Scripts/General/YarnBridge.cs
@@ -10,4 +10,40 @@ public class CutsceneYarnCommands : MonoBehaviour
     {
         AudioEvents.Play(soundId);
     }
+
+    [YarnFunction("top_three_scores")]
+    public static string TopThreeScores(
+        float media,
+        float jouranlism,
+        float proCom,
+        float perform,
+        float crIndustries,
+        float fashion,
+        float imgArts,
+        float gcm,
+        float intDesign
+    )
+    {
+        var scores = new Dicitonary<string, float>
+        {
+            { "Media", media },
+            { "Journalism", jouranlism },
+            { "ProCom", proCom },
+            { "Performance", perform },
+            { "Creative Industries", crIndustries },
+            { "Fashion", fashion },
+            { "Image Arts", imgArts },
+            { "GCM", gcm },
+            { "Interior Design", intDesign }
+        };
+
+        // Order descending and take top 3
+        var topThree = scores
+            .OrderByDescending(kv => kv.Value)
+            .Take(3)
+            .Select(kv => kv.Key);
+
+        // Return as a comma-separated string (easy for Yarn)
+        return string.Join(", ", topThree);
+    }
 }
